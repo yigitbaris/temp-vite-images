2ddac9d399434f5504a07afdd057fb08285b85a1
unknown <bariskaynakoglu01@gmail.com>
2023-08-04 15:56:58 +0300

    first commit

---
+++ src/SearchForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value
+    if (!searchValue) return
+    setSearchTerm(searchValue)
+  }
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form
+        className="search-form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button
+          type="submit"
+          className="btn"
+        >
+          search
+        </button>
+      </form>
+    </section>
+  )
+}
+export default SearchForm
